14e17859fa3fe6c621c476b7a514dc58eb940fa5
toskn <300899@gmail.com>
2017-09-12 11:35:01 +0300

    uploading two files done in class

---
+++ math.py
@@ -0,0 +1,59 @@
+Python 3.6.2 (v3.6.2:5fd33b5926, Jul 16 2017, 20:11:06) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+3*4
+12
+>>> 2**1
+2
+>>> 2**4
+16
+>>> 31/2
+15.5
+>>> 31//2
+15
+>>> print(paps)
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    print(paps)
+NameError: name 'paps' is not defined
+>>> print("aaa")
+aaa
+>>> name = input()
+hse privet
+>>> name = input()
+564
+>>> print(name**3)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    print(name**3)
+TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+>>> name = input()
+
+x= input()
+>>> s
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    s
+NameError: name 's' is not defined
+>>> print("x"**3)
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    print("x"**3)
+TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+>>> int x = input()
+SyntaxError: invalid syntax
+>>> x = input()
+34
+>>> int(x)
+34
+>>> x**34
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    x**34
+TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+>>> b=int(x)
+>>> b**3
+39304
+>>> 
---
+++ test.py
@@ -0,0 +1,6 @@
+print(5-10)
+print(3*7)
+print(2**16)
+print("Kak tebya zovut?")
+name=input()
+print("Privet, " + name + "!")
