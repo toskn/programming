dc5f68b4da80de8fb9682659be033792e83008c4
toskn <300899@gmail.com>
2017-10-01 21:05:40 +0300

    -

---
+++ Занятие 3/1.py
@@ -0,0 +1,8 @@
+s=32
+f=open("gaudeamus.txt")
+while s<=34:
+    s = s + 1
+    for x in f:
+            print(x)
+    pr
+
---
+++ Занятие 3/2.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = input()
+c="абвгдеёжзийклмнопрстуфхцчшщъыьэюя "
+len_c = len(c)
+ res=[]
+ for i in b
+ res.append(c[c.find(i)+a
