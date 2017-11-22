33af3bbeb26a6ff0faf25f042b12f38ec86b5fe6
toskn <300899@gmail.com>
2017-11-22 18:55:09 +0300

    uploaded 1st bonus task

---
+++ bonus/task1.py
@@ -0,0 +1,28 @@
+# Если пользователь вводит что-то кроме цифр, то значения будут разбиваться или удаляться.
+import re
+
+i = 0
+inputted = ""
+while i == 0:
+    a = input()
+    if a == "":
+        i += 1
+    else:
+        inputted = inputted + " " + a
+inputted = list(filter(None, re.split('\D', inputted)))
+inputted2 = []
+for ii in inputted:
+    if ii != '':
+        inputted2.append(int(ii))
+
+
+def sum_of_numbers(numbers):
+    if len(numbers) == 1:
+        return numbers[0]
+    else:
+        return numbers[0] + sum_of_numbers(numbers[1:])
+
+
+print((sum_of_numbers(inputted2))/len(inputted2))
+print(min(inputted2))
+print(max(inputted2))
