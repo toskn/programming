3335090103441ef64a5532899d183fb2be22b19a
toskn <300899@gmail.com>
2017-12-12 13:13:13 +0300

    -

--- EXAM/TASK2.py
+++ EXAM/TASK2.py
@@ -1,7 +1,13 @@
+ant_amount = 0
+b = []
 with open("/Users/egor/Documents/GitHub/programming.git/EXAM/Ozhegov.txt", encoding="utf-8") as text:
     text = text.read()
-    text = text.replace("|", " ")
     text = text.split("\n")
     for i in range(len(text)):
-        a = text[i].split()
-        if
+        a = text[i].split("|")
+        b.append(a)
+    for ii in range(len(b)):
+        if len(b[ii]) >= 3 and b[ii][2] != "":
+            ant_amount += 1
+print(ant_amount)
+
