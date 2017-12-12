f4fc419303876649663d3e547ed971dda5e24d88
toskn <300899@gmail.com>
2017-12-12 13:34:29 +0300

    -

---
+++ EXAM/TASK3.py
@@ -0,0 +1,25 @@
+i = 0
+inputted = ""
+b = []
+
+while i == 0:
+    a = input()
+    if a == "":
+        i += 1
+    else:
+        inputted = inputted + " " + a
+inputted = inputted.split()
+
+with open("/Users/egor/Documents/GitHub/programming.git/EXAM/Ozhegov.txt", encoding="utf-8") as text:
+    text = text.read()
+    text = text.replace("|", " | ")
+    text = text.split("\n")
+    for i in range(len(text)):
+        a = text[i].split(" ")
+        b.append(a)
+
+for ii in range(len(inputted)):
+    if inputted[ii] in b[ii][0] is True:
+        print(b[ii][0] + "-" + b[ii][3] + "-" + b[ii][1])
+    else:
+        print("there is no such a word in the dictionary")
